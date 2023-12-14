a56787244dea923e14652ac499e4ad181e38a5a5
EvanioTech <112532056+EvanioTech@users.noreply.github.com>
2023-12-14 10:11:21 -0300

    aula finalizada

--- src/components/cardnews.js
+++ src/components/cardnews.js
@@ -3,8 +3,87 @@ class cardnews extends HTMLElement {
     super() ;
 
     const shadow = this.attachShadow({mode: "open"});
-    shadow.innerHTML= "<h1> hello world</h1>" ;
+    shadow.appendChild(this.build());
+    shadow.appendChild(this.style());
+
  }   
+ build(){
+   const componentRoot = document.createElement("div");
+   componentRoot.setAttribute("class", "card")
+
+
+   const cardLeft = document.createElement("div");
+   cardLeft.setAttribute("class", "card_left");
+
+   const autor = document.createElement("span");
+   autor.textContent = "by " + (this.getAttribute("autor") || "anonymous");
+
+
+   const linktitle = document.createElement("a");
+   linktitle.textContent = this.getAttribute("title");
+   linktitle.href = this.getAttribute("link-url");
+
+   const newscontent = document.createElement("p");
+   newscontent.textContent = this.getAttribute("contet");
+
+
+   cardLeft.appendChild(autor);
+   cardLeft.appendChild(linktitle);
+   cardLeft.appendChild(newscontent);
+
+
+
+   const cardRight = document.createElement("div");
+   cardRight.setAttribute("class", "card_right");
+   const newsimage = document.createElement("img");
+   newsimage.src = this.getAttribute("foto");
+   newsimage.alt = "alternativa";
+   cardRight.appendChild(newsimage);
+
+   componentRoot.appendChild(cardLeft);
+   componentRoot.appendChild(cardRight);
+
+   return componentRoot;
+ }
+
+ style(){
+   const style = document.createElement("style")
+   style.textContent = `
+   
+   .card{
+      width: 80%;
+      box-shadow: 36px 19px 41px 10px rgba(0,0,0,0.88);
+  -webkit-box-shadow: 36px 19px 41px 10px rgba(0,0,0,0.88);
+  -moz-box-shadow: 36px 19px 41px 10px rgba(0,0,0,0.88);
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+     
+      margin: 30px
+  
+  }
+  
+  .card_left{
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      padding-left: 10px;
+  }
+  
+  .card_left > h1{
+  margin-top: 15px;
+  font-size: 25px;
+  }
+  .card_left >p{
+      color: darkgrey;
+  }
+   
+   
+   `
+
+
+   return style;
+ }
 }
 
 customElements.define('card-news' ,cardnews);
